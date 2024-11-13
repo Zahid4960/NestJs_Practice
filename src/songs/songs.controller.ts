90ed167882dfa71764b04edca9db33983c67c6ac
zahid.hasan <zahid.hasan@dreamonline.co.jp>
2024-11-13 10:40:24 +0600

    Injection Scopes

--- src/songs/songs.controller.ts
+++ src/songs/songs.controller.ts
@@ -1,9 +1,12 @@
-import { Body, Controller, Delete, Get, HttpException, HttpStatus, Inject, Param, ParseIntPipe, Post, Put } from '@nestjs/common';
+import { Body, Controller, Delete, Get, HttpException, HttpStatus, Inject, Param, ParseIntPipe, Post, Put, Scope } from '@nestjs/common';
 import { SongsService } from './songs.service';
 import { CreateSongDTO } from './dto/create-song-dto';
 import { Connection } from 'src/common/constants/connections';
 
-@Controller('songs')
+@Controller({
+    path: 'songs',
+    scope: Scope.REQUEST
+})
 export class SongsController {
 
     constructor(
