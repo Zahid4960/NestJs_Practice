8c744fee396e8f82df4712ded3eb80b122fa5d50
zahid.hasan <zahid.hasan@dreamonline.co.jp>
2024-11-14 12:02:17 +0600

    Authenticate User with Passport JWT

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,10 +3,21 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { UsersService } from 'src/users/users.service';
 import { UsersModule } from 'src/users/users.module';
+import { JwtModule } from '@nestjs/jwt';
+import { authConstants } from './auth.constants';
+import { JwtStrategy } from './jwt-strategy';
 
 @Module({
-  imports: [UsersModule],
-  providers: [AuthService],
+  imports: [
+    UsersModule, 
+    JwtModule.register({ 
+      secret: authConstants.secret, 
+      signOptions : {
+        expiresIn: '1d'
+      }
+    })
+  ],
+  providers: [AuthService, JwtStrategy],
   controllers: [AuthController],
   exports: [AuthService]
 })
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,19 +3,28 @@ import { User } from 'src/users/user.entity';
 import { UsersService } from 'src/users/users.service';
 import { LoginDTO } from './dto/login-dto';
 import * as bcrypt from 'bcryptjs';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-    constructor(private userService: UsersService) {}
+    constructor(
+        private jwtService: JwtService,
+        private userService: UsersService
+    ) {}
+    
 
-    async login(loginDTO: LoginDTO): Promise<User> {
+    async login(loginDTO: LoginDTO): Promise<{ accessToken: string }> {
         const user = await this.userService.findOne(loginDTO);
 
         const passwordMatched = await bcrypt.compare(loginDTO.password, user.password);
 
         if (passwordMatched) {
             delete user.password;
-            return user;
+            const payload = { email: user.email, sub: user.id };
+
+            return {
+                accessToken: this.jwtService.sign(payload)
+            };
         } else {
             throw new UnauthorizedException('Password does not match');
         }
